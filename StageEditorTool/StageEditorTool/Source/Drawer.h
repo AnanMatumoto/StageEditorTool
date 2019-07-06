8f70a0899c80b16cf7a1a966aa3ccbea0f45ae89
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-06 19:20:38 +0900

    ソースフォルダー作成・移動

---
+++ StageEditorTool/StageEditorTool/Source/Drawer.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Lib/DirectX9/DirectX9Device.h"
+#include <map>
+#include <string>
+
+class Drawer {
+
+public:
+
+	Drawer(
+		std::string tex_name,
+		float pos_x,
+		float pos_y,
+		float degree = 0.f,
+		float ofset_x = 0.f,
+		float ofset_y = 0.f,
+		float depth = 1.f,
+		DWORD color = 0x00ffffff
+	);
+
+
+
+private:
+};
---
+++ StageEditorTool/StageEditorTool/Source/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include "Lib/Lib.h"
+#include "Lib/Texture/TextureManager.h"
+#include "Drawer.h"
+#include <Windows.h>
+#include <string>
+
+
+int WINAPI WinMain(
+	HINSTANCE h_instance,
+	HINSTANCE h_pre_instance,
+	LPSTR	  lp_cmd_line,
+	INT       n_cmd_show
+) {
+
+	if (Lib::AppInit(1600, 980, "Test", false)==false) {
+		return 0;
+	}
+
+	while (Lib::ProcessMessage()){
+
+		Lib::DirectX9Device::GetInstance()->DrawStart();
+		Lib::TextureManager::GetInstance()->Load("./Res/object_64x64.png");
+		Drawer draw("./Res/object_64x64.png", 500, 500);
+		Lib::DirectX9Device::GetInstance()->DrawEnd();
+	}
+	Lib::AppEnd();
+	return 0;
+}
