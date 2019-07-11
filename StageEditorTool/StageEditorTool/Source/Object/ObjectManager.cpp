2716c76796268dc5a70a4e8b892116d674c7be7c
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-11 20:34:09 +0900

    オブジェクトマネージャー追加

---
+++ StageEditorTool/StageEditorTool/Source/Object/ObjectManager.cpp
@@ -0,0 +1,80 @@
+﻿#include "ObjectManager.h"
+#include "../File/File.h"
+#include  "../Factory/ObjectFactory.h"
+#include "../Object/Parameter/CollisionObjectParameter.h"
+#include "../Collision/CollisionDefinition.h"
+#include "../Collision/CollisionManager.h"
+
+void ObjectManager::LoadData(const char* file_name) {
+
+	if (File::InputMapData(file_name, m_map_list) == false)
+	{
+		return;
+	}
+
+	AddObject();
+}
+
+void ObjectManager::AddObject() {
+	
+	ObjectFactory factory;
+	//マウスオブジェクト作成
+	CollisionObjectParameter mouse_param(
+		CollisionType::POINT_TYPE,
+		"",
+		0.f, 0.f
+	);
+	m_object_list.emplace_back(factory.CreateMaouseObject(mouse_param));
+
+	if (m_map_list.empty()==false) {
+		//マップオブジェクト作成	
+		for (int i = 0; i < m_map_list.size(); ++i) {
+			m_object_list.emplace_back(factory.CreateMapObject(m_map_list[i]));
+		}
+	}
+}
+
+void ObjectManager::Init() {
+
+	
+	for (auto obj : m_object_list) {
+
+		if (obj == nullptr) {
+			return;
+		}
+		else if (obj->IsDelete() == true) {
+			continue;
+		}
+		obj->Init();
+	}
+}
+
+void ObjectManager::Update() {
+
+	for (auto obj : m_object_list) {
+
+		if (obj == nullptr) {
+			return;
+		}
+		else if (obj->IsDelete() == true) {
+			continue;
+		}
+		obj->Update();
+	}
+
+	CollisionManager::GetInstance()->Update();
+}
+
+void ObjectManager::Draw() {
+
+	for (auto obj : m_object_list) {
+
+		if (obj == nullptr) {
+			return;
+		}
+		else if (obj->IsDelete() == true) {
+			continue;
+		}
+		obj->Draw();
+	}
+}
---
+++ StageEditorTool/StageEditorTool/Source/Object/ObjectManager.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Object.h"
+#include "Parameter/MapObjectParameter.h"
+#include "Parameter/SpriteObjectParameter.h"
+#include <vector>
+
+class ObjectManager {
+
+public:
+	
+	static ObjectManager* GetInstance() {
+		static ObjectManager instance;
+		return &instance;
+	}
+
+	void LoadData(const char* file_name);
+	
+	void AddObject();
+
+	void Init();
+
+	void Update();
+
+	void Draw();
+
+private:
+
+	std::vector<Object*>m_object_list;
+	std::vector<MapObjectParameter>m_map_list;
+	std::vector<SpriteObjectParameter>m_sprite_list;
+};
