d140bf703a6c2394df5ca3e84fa2f6568a7ddfdd
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-11 20:34:59 +0900

    オブジェクト生成クラス作成

---
+++ StageEditorTool/StageEditorTool/Source/Factory/ObjectFactory.cpp
@@ -0,0 +1,22 @@
+﻿#include "ObjectFactory.h"
+#include "../Object/MapObject.h"
+#include "../Object/CollisionObject.h"
+#include "../Object/MouseObject.h"
+
+Object* ObjectFactory::CreteSprite(SpriteObjectParameter& param) {
+
+	Object* obj = new SpriteObject(param);
+	return obj;
+}
+
+Object* ObjectFactory::CreateMapObject(MapObjectParameter& param) {
+
+	Object* obj = new MapObject(param);
+	return obj;
+}
+
+Object* ObjectFactory::CreateMaouseObject(CollisionObjectParameter& param){
+
+	Object* obj = new MouseObject(param);
+	return obj;
+}
---
+++ StageEditorTool/StageEditorTool/Source/Factory/ObjectFactory.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "../Object/Object.h"
+#include "../Object/Parameter/CreateParameter.h"
+#include "../Object/Parameter/MapObjectParameter.h"
+#include "../Object/parameter/CollisionObjectParameter.h"
+
+class MapObject;
+class CollisionObject;
+
+
+class ObjectFactory {
+
+public:
+
+	Object* CreteSprite(SpriteObjectParameter& param);
+
+	Object* CreateMapObject(MapObjectParameter& param);
+
+	Object* CreateMaouseObject(CollisionObjectParameter& param);
+};
+
