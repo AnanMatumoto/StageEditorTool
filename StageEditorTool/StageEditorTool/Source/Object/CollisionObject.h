9a07af5eca56e099ce7498a7996350cf153f667a
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-17 21:16:31 +0900

    ｚ座標取得用ゲッター追加

--- StageEditorTool/StageEditorTool/Source/Object/CollisionObject.h
+++ StageEditorTool/StageEditorTool/Source/Object/CollisionObject.h
@@ -17,7 +17,8 @@ public:
 		float x, float y, float z= 1.f,
 		float rot=0.f, float scale_x=1.f, float scale_y=1.f
 	):SpriteObject(sprite_name,x, y, z , rot, scale_x, scale_y),
-	  m_type(type)
+	  m_type(type),
+	  m_is_hit(false)
 	{
 	}
 
@@ -35,10 +36,17 @@ public:
 	virtual void SetCollider(PointCollider& collider){
 	}
 
+	virtual float GetColliderDepth() {
+		return 0;
+	}
+
 	virtual void Reflection(CollisionObject* obj) = 0;
+
+
 
 protected:
 	CollisionType m_type;
+    bool          m_is_hit;
 
 private:
 	Collider m_collider;
