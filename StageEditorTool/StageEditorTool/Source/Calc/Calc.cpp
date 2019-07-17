319f568606f92d497d5ab48ca7ac5b720b31589e
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-17 21:15:57 +0900

    計算処理関数追加

---
+++ StageEditorTool/StageEditorTool/Source/Calc/Calc.cpp
@@ -0,0 +1,17 @@
+﻿#include "Calc.h"
+
+namespace Calc {
+
+	void SwapFloatValue(
+		float& value_1,
+		float& value_2
+	){
+		float temp = value_1;
+		value_1 = value_2;
+		value_2 = temp;
+	}
+
+	float PowFloat(float value) {
+		return value * value;
+	}
+}
---
+++ StageEditorTool/StageEditorTool/Source/Calc/Calc.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace Calc {
+
+	void SwapFloatValue(
+		float&value_1,
+		float&value_2
+	);
+
+	float PowFloat(float value);
+
+}
