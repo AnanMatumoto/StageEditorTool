c149dc3c9fba3183e50374081b645fe94e62a4f3
AnanMatumoto <matumoto.anan@gmail.com>
2019-07-20 15:33:24 +0900

    リソースクラス作成

---
+++ StageEditorTool/StageEditorTool/Source/Object/ResourceObject.cpp
@@ -0,0 +1,55 @@
+﻿#include "ResourceObject.h"
+#include "../Common/Common.h"
+#include "../Collision/CollisionManager.h"
+#include "../Lib/Input/Input.h"
+#include "../Editor/Editor.h"
+#include "../Factory/ObjectFactory.h"
+
+ResourceObject::ResourceObject(CollisionObjectParameter& data)
+	:CollisionObject(
+		data.object_type,
+		data.collision_type,
+		data.sprite_name,
+		data.pos.x,
+		data.pos.y,
+		data.pos.z,
+		data.rot,
+		data.scale_x,
+		data.scale_x
+	){
+	m_size = Lib::TextureManager::GetInstance()->GetSize(m_sprite_name);
+}
+
+void ResourceObject::Init() {
+
+    CollisionManager::GetInstance()->Entry(this);
+	m_collider.pos  = m_pos;
+	m_collider.size = m_size;
+}
+
+void ResourceObject::Update() {
+
+	
+	
+}
+
+void ResourceObject::Reflection(CollisionObject* obj) {
+
+	if (Lib::Input::GetInstance()->OnMousePush(LEFT)) {
+
+		//オブジェクトを生成
+		MapObjectParameter param(
+			ObjectType::MAP,
+			m_collision_type,
+			m_sprite_name,
+			WINDOW_CENTER_X,
+			WINDOW_CENTER_Y,
+			1.f
+		);
+		ObjectFactory factory;
+		factory.CreateMapObject(param);
+	}
+	else {
+		return;
+	}
+}
---
+++ StageEditorTool/StageEditorTool/Source/Object/ResourceObject.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "CollisionObject.h"
+#include "../Collision/CollisionDefinition.h"
+#include "Parameter/CollisionObjectParameter.h"
+#include "Parameter/MapObjectParameter.h"
+
+class ResourceObject :public CollisionObject {
+
+public:
+	ResourceObject(CollisionObjectParameter& param);
+
+	ResourceObject(
+		ObjectType object_type,
+		CollisionType collision_type,
+		std::string sprite_name,
+		float x, float y, float z = 1.f,
+		float scale_x = 1.f, float scale_y = 1.f
+	) :CollisionObject(
+		object_type,
+		collision_type,
+		sprite_name,
+		x, y, z,
+		scale_x,
+		scale_y)
+	{
+	}
+	
+	~ResourceObject()override
+	{
+	}
+
+	void Init()override;
+
+	void Update()override;
+
+	void SetCollider(RectCollider& collider)override {
+		collider = m_collider;
+	}
+
+	void Reflection(CollisionObject* obj)override;
+
+	const MapObjectParameter GetSelectResource()const {
+		return m_map_parameter;
+	}
+
+private:
+	RectCollider m_collider;
+	MapObjectParameter m_map_parameter;
+};
